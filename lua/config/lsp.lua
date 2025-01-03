7ecb20cfa1d10403c7d16391617e707a13472937
Lauro Caetano <laurocaetano1@gmail.com>
2025-01-04 00:48:21 +0100

    Add go-vim

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -12,3 +12,20 @@ require('mason-lspconfig').setup({
     -- A list of servers to automatically install if they're not already installed
     ensure_installed = { 'gopls', 'lua_ls' },
 })
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+local lspconfig = require("lspconfig")
+lspconfig.gopls.setup({
+  capabilities = capabilities,
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
+      gofumpt = true,
+    },
+  },
+})
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,5 +3,8 @@ return {
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
+  },
+  {
+    "fatih/vim-go",
   }
 }
